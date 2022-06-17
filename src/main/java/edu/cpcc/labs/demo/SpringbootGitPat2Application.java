81df15fdf470586cb3ac9677b344b5b7ffcdafec
PTsishuk <tishukp@gmail.com>
2022-06-17 13:26:09 -0400

    initial release

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitPat2Application.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitPat2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitPat2Application.class, args);
+	}
+
+}
